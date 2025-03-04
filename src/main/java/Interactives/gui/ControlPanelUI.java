cdf25b3a3ae0613bf41ab8b98ffc80dd70bd995b
yocumnatwit <yocumn@wit.edu>
2025-03-04 16:38:20 -0500

    Add initial implementations for ControlPanelUI, PortScanner, ThreadHandler, and TrafficMonitor classes

---
+++ src/main/java/Interactives/gui/ControlPanelUI.java
@@ -0,0 +1,23 @@
+package Interactives.gui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ * This class represents the Control Panel UI for the firewall project.
+ * It utilizes JavaFX to create a graphical user interface.
+ */
+public class ControlPanelUI extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+    public static void launchControlPanel(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/components/trafficmonitor/TrafficMonitor.java
@@ -0,0 +1,4 @@
+package components.trafficmonitor;
+
+public class TrafficMonitor {
+}
