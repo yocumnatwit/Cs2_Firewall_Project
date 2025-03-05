2546ef50987b4086b594fde1905dcdd0ebb41499
Dylan Lamb <lambd1@wit.edu>
2025-03-05 11:52:23 -0500

    javaFX up and running

--- src/main/java/Interactives/gui/ControlPanelUI.java
+++ src/main/java/Interactives/gui/ControlPanelUI.java
@@ -1,10 +1,11 @@
 package Interactives.gui;
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.Stage;
+import javafx.scene.canvas.Canvas;
 
 /**
  * This class represents the Control Panel UI for the firewall project.
@@ -12,9 +13,27 @@ import javafx.stage.Stage;
  */
 public class ControlPanelUI extends Application {
 
-    @Override
-    public void start(Stage primaryStage) {
 
+    private int width = 800; //setting default values for now
+    private int height = 800;
+    private String windowName = "JavaFireWall";
+
+    @Override
+    public void start(Stage stage) {
+
+        //Setting up basic javaFX stuff
+        Canvas canvas = new Canvas(width, height);
+        stage.setTitle(windowName);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        Group group = new Group(canvas);
+        Scene scene = new Scene(group, width, height);
+        stage.setScene(scene);
+
+
+
+
+
+        stage.show();
     }
 
     public static void launchControlPanel(String[] args) {
