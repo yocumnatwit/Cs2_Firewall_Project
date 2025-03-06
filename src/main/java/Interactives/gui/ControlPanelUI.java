d01a4d35482334d590d212eef0fa4a9f4cb2f84e
Dylan Lamb <lambd1@wit.edu>
2025-03-06 13:15:04 -0500

    javaFX up and running

--- src/main/java/Interactives/gui/ControlPanelUI.java
+++ src/main/java/Interactives/gui/ControlPanelUI.java
@@ -4,8 +4,12 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.shape.Rectangle;
+import java.util.ArrayList;
+
 
 /**
  * This class represents the Control Panel UI for the firewall project.
@@ -14,26 +18,66 @@ import javafx.scene.canvas.Canvas;
 public class ControlPanelUI extends Application {
 
 
-    private int width = 800; //setting default values for now
-    private int height = 800;
+    private int width = 1280; //setting default values for now
+    private int height = 960;
     private String windowName = "JavaFireWall";
+
+    //JavaFX objects
+    private Group group;
+    private Scene scene;
+    private GraphicsContext gc;
+    private Canvas canvas;
+
 
     @Override
     public void start(Stage stage) {
 
         //Setting up basic javaFX stuff
-        Canvas canvas = new Canvas(width, height);
+        canvas = new Canvas(width, height);
         stage.setTitle(windowName);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        Group group = new Group(canvas);
-        Scene scene = new Scene(group, width, height);
+        gc = canvas.getGraphicsContext2D();
+        group = new Group(canvas); //Group acts as root
+        scene = new Scene(group, width, height);
         stage.setScene(scene);
 
 
-
+        //Actually showing things to the screen
+        drawBackground();
 
 
         stage.show();
+    }
+
+    private void drawBackground(){
+        double canvasWidth = canvas.getWidth();
+        double canvasHeight = canvas.getHeight();
+        int buffer = (int) ((canvasWidth + canvasHeight) / 80);
+
+
+        //Fill Grey in background
+        gc.setFill(Color.GRAY);
+        gc.fillRect(0, 0, canvasWidth, canvasHeight);
+
+        //Add places for text boxes
+        ArrayList<Rectangle> textPlacements = new ArrayList<>();
+        textPlacements.add(new Rectangle((canvasWidth / 4) + buffer, buffer,(canvasWidth / 2) - (buffer * 2), (canvasHeight * 5 / 8))); //Add main textbox
+        textPlacements.add(new Rectangle(buffer, buffer, ((canvasWidth / 4) - buffer), canvasHeight / 4)); //Adds Left Textbox unsure of what is going there for now
+        textPlacements.add(new Rectangle((canvasWidth * 3 / 4), buffer, ((canvasWidth / 4) - buffer ), canvasHeight / 4));
+
+        //Set the color of all textplacements to black and round corners
+        for (int i = 0; i < textPlacements.size(); i++){
+            textPlacements.get(i).setFill(Color.BLACK);
+            textPlacements.get(i).setArcWidth(20);
+            textPlacements.get(i).setArcHeight(20);
+        }
+
+
+        group.getChildren().addAll(textPlacements);
+    }
+
+    //clears the background
+    private void clearUI(Group group){
+        group.getChildren().removeAll();
     }
 
     public static void launchControlPanel(String[] args) {
