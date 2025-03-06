efe9657f3d230185bad3a216db032b20f9b345dc
Dylan Lamb <lambd1@wit.edu>
2025-03-06 13:52:21 -0500

    Added text to panels

--- src/main/java/Interactives/gui/ControlPanelUI.java
+++ src/main/java/Interactives/gui/ControlPanelUI.java
@@ -5,10 +5,13 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import java.util.ArrayList;
+
 
 
 /**
@@ -60,24 +63,54 @@ public class ControlPanelUI extends Application {
 
         //Add places for text boxes
         ArrayList<Rectangle> textPlacements = new ArrayList<>();
-        textPlacements.add(new Rectangle((canvasWidth / 4) + buffer, buffer,(canvasWidth / 2) - (buffer * 2), (canvasHeight * 5 / 8))); //Add main textbox
-        textPlacements.add(new Rectangle(buffer, buffer, ((canvasWidth / 4) - buffer), canvasHeight / 4)); //Adds Left Textbox unsure of what is going there for now
-        textPlacements.add(new Rectangle((canvasWidth * 3 / 4), buffer, ((canvasWidth / 4) - buffer ), canvasHeight / 4));
+        textPlacements.add(new Rectangle((canvasWidth / 4) + buffer, buffer, (canvasWidth / 2) - (buffer * 2), canvasHeight * 5 / 8)); // Main textbox
+        textPlacements.add(new Rectangle(buffer, buffer, (canvasWidth / 4) - buffer, canvasHeight / 4)); // Left textbox
+        textPlacements.add(new Rectangle((canvasWidth * 3 / 4), buffer, (canvasWidth / 4) - buffer, canvasHeight / 4)); // Right textbox
 
         //Set the color of all textplacements to black and round corners
-        for (int i = 0; i < textPlacements.size(); i++){
-            textPlacements.get(i).setFill(Color.BLACK);
-            textPlacements.get(i).setArcWidth(20);
-            textPlacements.get(i).setArcHeight(20);
+        for (Rectangle rect : textPlacements) {
+            rect.setFill(Color.BLACK);
+            rect.setArcWidth(20);
+            rect.setArcHeight(20);
         }
 
+        //Add all labels for text boxes
+        ArrayList<Text> panelLabels = new ArrayList<>();
+        double textOffsetY = 20;
+        panelLabels.add(new Text(((canvasWidth / 4) + buffer) + ((canvasWidth / 2) - (buffer * 2)) / 2, buffer + textOffsetY, "Main Panel")); // Main panel label
+        panelLabels.add(new Text(buffer + ((canvasWidth / 4) - buffer) / 2, buffer + textOffsetY, "Placeholder Panel")); // Left panel label
+        panelLabels.add(new Text((canvasWidth * 3 / 4) + ((canvasWidth / 4) - buffer) / 2, buffer + textOffsetY, "Warning Panel")); // Right panel label
 
+
+
+
+
+
+        //Change all text to green and possibly change font in the future
+        for (Text text: panelLabels){
+            text.setFill(Color.GREEN);
+            text.setTextAlignment(TextAlignment.CENTER);
+            centerText(text);
+        }
+
+        //Add all collections to group
         group.getChildren().addAll(textPlacements);
+        group.getChildren().addAll(panelLabels);
     }
 
     //clears the background
     private void clearUI(Group group){
         group.getChildren().removeAll();
+    }
+
+
+    //Centers Text around its x and y location
+    private void centerText(Text text) {
+        double textWidth = text.getBoundsInLocal().getWidth();
+        double textHeight = text.getBoundsInLocal().getHeight();
+
+        text.setX(text.getX() - (textWidth / 2));
+        text.setY(text.getY() - (textHeight / 4));
     }
 
     public static void launchControlPanel(String[] args) {
