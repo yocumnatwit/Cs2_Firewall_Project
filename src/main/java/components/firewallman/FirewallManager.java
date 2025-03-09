fb53867c00de73b2b0240512af0b013e53d6ed8d
Kailyn Dykert <dykertk@wit.edu>
2025-03-09 18:19:50 -0400

    Updated FirewallManager to use netstat and created skeleton for ThreadHandler.

--- src/main/java/components/firewallman/FirewallManager.java
+++ src/main/java/components/firewallman/FirewallManager.java
@@ -12,6 +12,8 @@ public class FirewallManager {
 	public boolean firewallStatus;
 	private Blocklist blockedIPs;
 	private ArrayList<Integer> openPorts;
+	
+	// TODO: Take a look at how this should be used. Currently no function can use it.
 	private ArrayList<Integer> allowedPorts;
 		
 	public FirewallManager() {
@@ -69,6 +71,7 @@ public class FirewallManager {
 	}
 		
 	public boolean checkBlockedIP(String ip) {
+		// TODO: Should we check blocked port instead?
 		// TODO: Need to have a way to define id and name.
 		String id = ip;
 		String name = "";
@@ -101,25 +104,60 @@ public class FirewallManager {
 	}
 		
 	public void scanPorts() {
-		// TODO: Re-implement, This will take a long amount of time.
-		Socket portCheck;
-		String host = "localhost";
-		ArrayList<Integer> openPorts = new ArrayList<>();
+		int lineCount = 0;
 		
-		for (int port = 1; port < 65535; port++) {
-			try {
-				portCheck = new Socket(host, port);
-				openPorts.add(port);
+		// https://askubuntu.com/questions/538443/whats-the-difference-between-port-status-listening-time-wait-close-wait
+		try {
+			Process process = new ProcessBuilder("netstat", "-antd").start();
+			
+			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			String line;
+			
+			
+			// Process each line
+			while(((line = reader.readLine()) != null)) {
+				// Skip first two lines
+				if (lineCount <= 1) {
+					continue;
+				}
+				
+				// Split by whitespace
+				String[] results = line.split("\\s+");
+				
+				// Line may be processed wrong so we should skip
+				if (results.length != 6) {
+					continue;
+				}
+				
+				String state = results[5];
+				if (state.equals("CLOSE")) {
+					continue;
+				}
+				
+				String localAddress = results[3];
+				String portString = localAddress.split(":")[1];
+				
+				try {
+					int port = Integer.parseInt(portString);
+					this.openPorts.add(port);
+				} catch (NumberFormatException e) {
+					continue;
+				}
+				
+				
 			}
-			catch (UnknownHostException e) { break; }
-			catch (IOException e) {}
+			
+			reader.close();
+			process.waitFor();
+			
+		} catch(IOException | InterruptedException e) {
+			// TODO: Possibly implement some way to show that scanPorts failed.
+			// TODO: Add error in-case netstat is not installed.
+			return;
 		}
-		
-		this.openPorts = openPorts;
 	}
 		
 	private void sendWarning(String message) {
-			
 	}
 		
 }
--- src/main/java/components/threadhandler/ThreadHandler.java
+++ src/main/java/components/threadhandler/ThreadHandler.java
@@ -1,8 +1,46 @@
 package components.threadhandler;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+// TODO: Still must be completed.
 public class ThreadHandler extends Thread{
 
     private final int CORECOUNT = Runtime.getRuntime().availableProcessors();
     private final int MAXTHREADS = CORECOUNT * 2;
+    
+    private int currentThreads = 0;
+    private ArrayList<Process> runningProcesses = new ArrayList<>();
 
+    //+ run(process: String): void
+    public void run(String process) {
+    	if (currentThreads == MAXTHREADS) {
+    		return;
+    	}
+    	
+        ProcessBuilder builder = new ProcessBuilder(process);
+        try {
+        	Process proc = builder.start();
+        	runningProcesses.add(proc);
+        	currentThreads++;
+        	
+        } catch(IOException e) {}
+    }
+    //+ run(process: String, port: int): void
+    public void run(String process, int Port) {
+    	
+    }
+    //+ pauseThread(process: String): void
+    public void pauseThread(String process) {
+    	
+    }
+    //+ killThread(process: String)
+    public void killThread(String process) {
+    	
+    }
+    //+ printThread(): void
+    public void printThread() {
+    	
+    }
+    
 }
