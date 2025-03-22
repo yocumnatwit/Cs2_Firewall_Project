af6cf1bea4135efce6b9db4f7ab8dc8f26a9275b
yocumnatwit <yocumn@wit.edu>
2025-03-22 15:51:24 -0400

    Merge Commit

--- src/main/java/components/warnman/WarningManager.java
+++ src/main/java/components/warnman/WarningManager.java
@@ -50,7 +50,7 @@ public class WarningManager {
        warninglogBuffer = new StringBuffer();
     }
 
-    /**
+    /**q
      * This method writes a warning to the warninglogBuffer.
      * @param warning The warning to be written to the buffer.
      **/
@@ -116,6 +116,7 @@ public class WarningManager {
      * @return The content of the warning log file as a string, or null if an error occurs while reading the file.
      */
     public String readLogs() {
+        checkFileExistence();
         try {
             return FileUtils.readFileToString(warninglogFile, "UTF-8");
         } catch (IOException e) {
